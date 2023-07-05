24991ab6ec0a901f08a6ed2f92e2bbce686129ea
Amitesh Tripathi <theamiteshtripathi@gmail.com>
2023-07-05 14:56:13 -0400

    corrected the package name

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "TextSummarizer"
+project_name = "textSummarizer"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
